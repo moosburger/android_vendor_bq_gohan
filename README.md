8fea5d308885741d5c6490b8c8b83da32868557b
moosburger <moosburger@freenet.de>
2021-05-06 07:09:58 +0200

    Modified how to copy *_ini.so Files

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # android_vendor_bq_gohan
-Version 1.1.1
+Version 1.1.2
 
+### Modified how to copy *_ini.so Files
 ### Ims Files and some more permissions
 ### Bluetooth Driver from Stock
--- imsBq/vendor/app/ims/Android.mk
+++ imsBq/vendor/app/ims/Android.mk
@@ -11,9 +11,12 @@ src_proto := src
 
 LOCAL_PATH:= $(call my-dir)
 
-$(shell mkdir -p $(TARGET_OUT)/vendor/lib)
-$(shell cp $(LOCAL_PATH)/lib/libimsmedia_jni.so $(TARGET_OUT)/vendor/lib/libimsmedia_jni.so)
-$(shell cp $(LOCAL_PATH)/lib/libimscamera_jni.so $(TARGET_OUT)/vendor/lib/libimscamera_jni.so)
+#$(shell mkdir -p $(TARGET_OUT)/vendor/lib)
+#$(shell cp $(LOCAL_PATH)/lib/libimsmedia_jni.so $(TARGET_OUT)/vendor/lib/libimsmedia_jni.so)
+#$(shell cp $(LOCAL_PATH)/lib/libimscamera_jni.so $(TARGET_OUT)/vendor/lib/libimscamera_jni.so)
+#$(shell mkdir -p $(TARGET_OUT)/system/lib)
+#$(shell cp $(LOCAL_PATH)/lib/libimsmedia_jni.so $(TARGET_OUT)/system/lib/libimsmedia_jni.so)
+#$(shell cp $(LOCAL_PATH)/lib/libimscamera_jni.so $(TARGET_OUT)/system/lib/libimscamera_jni.so)
 
 # ==========================================================================
 # Build the service
@@ -36,8 +39,8 @@ LOCAL_PROTOC_OPTIMIZE_TYPE := micro
 LOCAL_SRC_FILES := $(call all-java-files-under, $(src_java_ims)) \
     $(call all-proto-files-under, $(src_proto))
 
-#LOCAL_REQUIRED_MODULES := libimsmedia_jni libimscamera_jni
-#LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_APPS)
+LOCAL_REQUIRED_MODULES := libimsmedia_jni libimscamera_jni
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_APPS)
 
 include $(BUILD_PACKAGE)
 
