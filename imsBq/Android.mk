55b6b2c071387851147f21a807ad8aa68b28c86b
moosburger <moosburger@freenet.de>
2022-11-14 15:17:12 +0100

    make updated

--- imsBq/Android.mk
+++ imsBq/Android.mk
@@ -9,8 +9,8 @@ $(shell cp $(LOCAL_PATH)/radio.te $(TARGET_DEVICE_DIR)/sepolicy/system.prop)
 #$(shell cp $(LOCAL_PATH)/vendor/bq/gohan/imsBq/vendor/app/ims/lib/libimsmedia_jni.so $(TARGET_OUT)/vendor/lib/libimsmedia_jni.so)
 #$(shell cp $(LOCAL_PATH)/vendor/bq/gohan/imsBq/vendor/app/ims/lib/libimscamera_jni.so $(TARGET_OUT)/vendor/lib/libimscamera_jni.so)
 $(shell mkdir -p $(TARGET_OUT)/system/lib)
-$(shell cp $(LOCAL_PATH)/vendor/bq/gohan/imsBq/vendor/app/ims/lib/libimsmedia_jni.so $(TARGET_OUT)/system/lib/libimsmedia_jni.so)
-$(shell cp $(LOCAL_PATH)/vendor/bq/gohan/imsBq/vendor/app/ims/lib/libimscamera_jni.so $(TARGET_OUT)/system/lib/libimscamera_jni.so)
+$(shell cp $(LOCAL_PATH)/vendor/app/ims/lib/libimsmedia_jni.so $(TARGET_OUT)/system/lib/libimsmedia_jni.so)
+$(shell cp $(LOCAL_PATH)/vendor/app/ims/lib/libimscamera_jni.so $(TARGET_OUT)/system/lib/libimscamera_jni.so)
 
 # ==========================================================================
 
